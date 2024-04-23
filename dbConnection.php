305640cbc0c73f3ba3ff3575c6bd2290d21039dc
Plombercho <167874321+Plombercho@users.noreply.github.com>
2024-04-23 17:16:56 +0300

    dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -4,9 +4,9 @@ $user = 'autoentu';
 $password = 'f7PQz]z(eJ3P';
 $database = 'autoentu_db';
 
-$mysqli = new mysqli($server, $user, $password, $database);
+$conn = new mysqli($server, $user, $password, $database);
 
-if ($mysqli->connect_error) {
+if ($conn->connect_error) {
     echo 'Възникна грешка при връзката с базата данни';
 }
 ?>
