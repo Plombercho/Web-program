322bde61adb13ef77553de67f594fea4d33fe6d5
Plombercho <167874321+Plombercho@users.noreply.github.com>
2024-04-23 17:26:22 +0300

    paypal_payment.php

--- paypal_payment.php
+++ paypal_payment.php
@@ -7,36 +7,32 @@ $confirm_query_result=mysqli_query($conn,$confirm_query) or die(mysqli_error($co
 ?>
 
 <?php
-// Проверка за съществуване на POST заявката
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Проверка за съществуване на общата сума
+    // check if total amount and total quantity exist
     if(isset($_POST['total_amount']) && isset($_POST['total_quantity']) && is_numeric($_POST['total_amount'])) {
         $total_amount = $_POST['total_amount'];
         $total_quantity = $_POST['total_quantity'];
 
-        // Редирект към страницата на PayPal с необходимите параметри
-        $paypal_url = 'https://www.sandbox.paypal.com/cgi-bin/webscr'; // URL на страницата на PayPal за плащане
-        $business_email = 'viktor_veli@abv.bg'; // Тук посочете вашия PayPal имейл
 
-        // Генериране на данни за плащане към PayPal
+        $paypal_url = 'https://www.sandbox.paypal.com/cgi-bin/webscr'; // URL to PayPal paying page
+        $business_email = 'viktor_veli@abv.bg'; // PayPal mail
+
+        // Generate payment data to PayPal
         $data = array(
             'cmd' => '_xclick',
             'business' => $business_email,
             'amount' => $total_amount,
             'quantity' => $total_quantity,
-            'currency_code' => 'USD', // Валутен код
-            // Добавете още параметри по необходимост
+            'currency_code' => 'USD',
         );
 
-        // Подготовка на данните за пренасочване
+        // Preparing data for redirection
         $query_string = http_build_query($data);
         $paypal_redirect_url = $paypal_url . '?' . $query_string;
 
-        // Пренасочване към страницата на PayPal
         header("Location: $paypal_redirect_url");
         exit();
     } else {
-        // Ако общата сума липсва или не е числено значение, изведете грешка
         echo "Грешка: Невалидна обща сума за плащане.";
     }
 } else {
