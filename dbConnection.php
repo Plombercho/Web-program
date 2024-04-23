0620a136e8418663c444385e6f4d8013bee69870
Plombercho <167874321+Plombercho@users.noreply.github.com>
2024-04-23 17:12:32 +0300

    dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,11 +1,4 @@
 <?php
-//all the variables defined here are accessible in all the files that include this one/ this is the connection of the localhost
-$conn = mysqli_connect('localhost', 'root', '', 'diplomnadb') or die('connection failed');
-
-?>
-
-<?php
-/* hosted localhost information!
 $server = 'localhost';
 $user = 'autoentu';
 $password = 'f7PQz]z(eJ3P';
@@ -16,5 +9,4 @@ $mysqli = new mysqli($server, $user, $password, $database);
 if ($mysqli->connect_error) {
     echo 'Възникна грешка при връзката с базата данни';
 }
-*/
 ?>
