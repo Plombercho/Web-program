7bc8f4d730e109f10a2f06c7d0639b3b764250b6
Plombercho <167874321+Plombercho@users.noreply.github.com>
2024-04-23 17:27:32 +0300

    setting_script.php

--- setting_script.php
+++ setting_script.php
@@ -7,18 +7,14 @@ $old_password = mysqli_real_escape_string($conn, $_POST['oldPassword']);
 $new_password = mysqli_real_escape_string($conn, $_POST['newPassword']);
 $user_id = $_SESSION['user_id'];
 
-// Fetch the hashed password from the database
 $get_password_query = "SELECT password FROM users WHERE id='$user_id'";
 $get_password_result = mysqli_query($conn, $get_password_query) or die(mysqli_error($conn));
 $row = mysqli_fetch_assoc($get_password_result);
 $hashed_password = $row['password'];
 
-// Verify old password
 if (password_verify($old_password, $hashed_password)) {
-    // Hash the new password
     $hashed_new_password = password_hash($new_password, PASSWORD_DEFAULT);
 
-    // Update the password in the database
     $update_password_query = "UPDATE users SET password='$hashed_new_password' WHERE id='$user_id'";
     $update_password_result = mysqli_query($conn, $update_password_query) or die(mysqli_error($conn));
 
