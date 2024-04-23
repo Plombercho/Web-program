1562abcb060da0dfb3ccc6e9524851ebd3bb42b2
Plombercho <167874321+Plombercho@users.noreply.github.com>
2024-04-23 16:56:15 +0300

    admin_header.php

--- admin_header.php
+++ admin_header.php
@@ -24,13 +24,11 @@
 				
 				</div>
 					<div class="col">
-						<!-- Profile dropdown -->
 						<div class="dropdown">
 							<a style="font-size: 22px" href="#" id="profileDropdown" data-toggle="dropdown">
 								<span class="fa fa-user-circle-o">Профил</span>
 							</a>
 							<div class="dropdown-menu" aria-labelledby="profileDropdown">
-								<a href="settings.php" class="fa fa-cogs dropdown-item">Настройки</a>
 								<a href="logout.php" class="fa fa-sign-out dropdown-item">Излизане</a>
 							</div>
 						</div>
@@ -49,11 +47,9 @@
 		profileDropdown.addEventListener('click', function (event) {
 			event.preventDefault();
 			event.stopPropagation();
-			// Toggle dropdown menu
 			profileDropdown.nextElementSibling.classList.toggle('show');
 		});
 
-		// Close dropdown when clicking outside
 		document.addEventListener('click', function (event) {
 			if (!profileDropdown.nextElementSibling.contains(event.target)) {
 				profileDropdown.nextElementSibling.classList.remove('show');
