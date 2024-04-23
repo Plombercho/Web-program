06742070f5cdc68f33badd8cc715120b6265c9e9
Plombercho <167874321+Plombercho@users.noreply.github.com>
2024-04-23 17:28:02 +0300

    settings.php

--- settings.php
+++ settings.php
@@ -9,15 +9,10 @@ require 'dbConnection.php';
     <title>Settings</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- latest compiled and minified CSS -->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
-    <!-- jquery library -->
     <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
-    <!-- Latest compiled and minified javascript -->
     <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-    <!-- External CSS -->
     <link rel="stylesheet" href="css/style.css" type="text/css">
-    <!-- icon shows with this url-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
